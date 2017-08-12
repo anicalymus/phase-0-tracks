2ccc6781fa4caa0eb0f63bce5d9dc9b998b8d9eb
anicalymus <anicalymus@gmail.com>
2017-08-11 21:00:46 -0700

    Add decryption

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,5 +1,4 @@
-encrypted_word = "abc"
-
+#encrypt abc
 def encrypt_method(string)
   
   index = 0
@@ -13,3 +12,19 @@ def encrypt_method(string)
 end
 
 encrypt_method(encrypted_word)
+
+#decrypt bcd
+
+def decrypt_method(string)
+  
+index = 0
+alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+while index < string.length
+  current_letter = string[index]
+  current_index = alpha.index(current_letter)
+  new_letter = alpha[current_index-1]
+  string[index] = new_letter
+  index +=1
+  end
+  puts string
+end
