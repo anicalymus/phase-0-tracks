104db1c1c15dfa93959de6111758746ad870a3e4
anicalymus <anicalymus@gmail.com>
2017-08-13 09:05:40 -0700

    Add data to get to know the vampire

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+puts "What is your name?"
+vampire_name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+birth_year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+wants_garlic_bread = gets.chomp
+puts "Would you like to enroll in the company's health insurance? (y/n)"
+wants_health_insurance = gets.chomp
+
+
+
