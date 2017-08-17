b28cb848097c8cc933c171897594a1e0f18c155c
anicalymus <anicalymus@gmail.com>
2017-08-17 15:21:40 -0700

    Add hash to client info form

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -5,7 +5,7 @@ puts "Name:"
 client_name = gets.chomp
 
 puts "Age:"
-age = gets.chomp
+client_age = gets.chomp
 
 puts "How many children do you have?:"
 num_children = gets.chomp
@@ -14,7 +14,18 @@ puts "What is your decor theme?:"
 decor_theme = gets.chomp
 
 puts "Here is your client info:"
-puts client_name
-puts age
-puts num_children
-puts decor_theme
+
+#Converting user input to the a hash
+
+client_info = {
+	name: client_name,
+	age: client_age,
+	children: num_children,
+	decor: decor_theme
+}
+
+p client_info
+
+#Print the hash back out to the screen when the designer has answered all of the questions.
+#Give the user the opportunity to update a key (no need to loop, once is fine). After all, sometimes users make mistakes! If the designer says "none", skip it. But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
+#Print the latest version of the hash, and exit the program.
