d247124958601d69da6308076ac189145abcb394
anicalymus <anicalymus@gmail.com>
2017-08-17 13:48:03 -0700

    Add client info

---
+++ ruby/hashes.rb
@@ -0,0 +1,20 @@
+#Creating a client info sheet
+puts "Interior design client information form"
+
+puts "Name:"
+client_name = gets.chomp
+
+puts "Age:"
+age = gets.chomp
+
+puts "How many children do you have?:"
+num_children = gets.chomp
+
+puts "What is your decor theme?:"
+decor_theme = gets.chomp
+
+puts "Here is your client info:"
+puts client_name
+puts age
+puts num_children
+puts decor_theme
