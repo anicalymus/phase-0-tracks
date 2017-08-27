be5add260d1cfbdc46213ea07bd9c6c0b7b8dc95
anicalymus <anicalymus@gmail.com>
2017-08-27 09:19:51 -0700

    Add new santas with different genders, ethnicitites and age

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,14 @@
 class Santa
+	attr_reader :ethnicity, :age
+	attr_accessor :gender
+
+	def initialize(gender, ethnicity, age)
+		@gender = gender
+		@ethnicity = ethnicity
+		@age = age.to_i
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@celebrate_birthday = 1
+	end
 
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
@@ -8,15 +18,76 @@ class Santa
 		puts "That was a good #{cookie_type} cookie!"
 	end
 
-	def initialize
-		puts "initializing Santa instance..."
+	def celebrate_birthday=(celebrate_birthday)
+		@celebrate_birthday = @celebrate_birthday
+		puts "Santa is #{celebrate_birthday} today, happy birthday!"
 	end
 
+	def get_mad_at(reindeer_name)
+		@reindeer_ranking.delete_if {|reindeer| reindeer == reindeer_name}
+		@reindeer_ranking.insert(-1, reindeer_name)
+		puts "Bad #{@reindeer_ranking[-1]}...I am mad at you!"
+	end
+
+	def about
+		puts "New santa facts:"
+		puts "Gender: #{@gender}"
+		puts "Ethnicity: #{@ethnicity}"
+		puts "Age: #{@age}"
+		
+	end
+
+	santas = []
+	example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+	example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+	#counter = 0
+	while santas.length < 50
+		santas << Santa.new(example_genders[rand 0..7], example_ethnicities[rand 0..7], (rand 0..140))
+	end
+# 	santas.each do |santa|
+# 		santa.ethnicity
+# 		santa.gender
+# 	end
+santas.each do |index|
+	index.about
 end
+ end
+
+
+#setter method
+	# def gender=(new_gender)
+	# 	@gender = new_gender
+	#  end
+# getter methods
+# 	def age
+# 		  @age = @age + 1
+# 		  puts "Santa is #{@age} year old!"
+# 	end
+
+	# def ethnicity
+	# 	 @ethnicity = "black"
+	# 	 puts "Santa is a #{@ethnicity} #{@gender}!"
+	# end
+
+
+	#def initialize
+	#	puts "initializing Santa instance..."
+	#end
+
+
 
 
 
 #DRIVER CODE
-santa = Santa.new
+santa = Santa.new("female", "black", "1")
 santa.speak
 santa.eat_milk_and_cookies("oatmeal rasin")
+santa.gender = "female"
+santa.age 
+santa.ethnicity 
+santa.celebrate_birthday = "1"
+santa.get_mad_at("Vixen")
+santa.about
+
+
