66de7b68f0c9c999e7ed97a7f30e702424d7bac2
anicalymus <anicalymus@gmail.com>
2017-08-30 17:30:16 -0700

    Reverse a string

--- js/explore.js
+++ js/explore.js
@@ -1,5 +1,14 @@
 // Creating a function that will reverse a string. In the parenthesis
 //is the placeholder for the string that will be reversed. 
 function reverseString(str) {
-
+// Splitting the string into an array of letters
+	var splitString = str.split(''); 
+// Taking the result of the array of letters and reversing them
+	var reverseArray = splitString.reverse();
+// Joining the the string that was split into an array of letters and the reversed array.
+	var joinArray = reverseArray.join('');
+// Returning the array that was joined together. This is the string reversed.
+  return joinArray;
 }
+// Printing the the original string but reversed.
+console.log(reverseString("outside"));
