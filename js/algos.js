47bfcd09dc4da6bbaa1bcfde0a4177087b501352
anicalymus <anicalymus@gmail.com>
2017-09-03 21:20:40 -0700

    Compare key-value pairs

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,4 @@
+// Release 0: Find the Longest Phrase
 var Largest  = function(arrOfStrings) {
 		arrOfStrings.forEach(function(item) {
 			if (largestInArray.length < item.length); {
@@ -10,8 +11,21 @@ var largestInArray = ["let's eat food", "i like lasagna", "let's go to mountain"
 
 Largest(largestInArray)
 
-console.log("_____________________________________");
 
-var one = ({day: "sunny", night: "dark"}, {day: "cloudy", night: "dark"})
-var two = ({clothes: "shirt", fruit: "apple"}, {clothes: "pants", weather: "hot"}
+// Release 1: Find a Key-Value Match
+function checkKeyValPair(first, second){
+  for (var key in first){
+    if (first[key] === second[key]){
+      return true;
+    }
+  }
+	return false;
+}
+// checkKeyValPair(first, second)
+
+// Driver Code
+checkKeyValPair({day: "sunny", night: "dark"}, {day: "cloudy", night: "dark"});
+
+checkKeyValPair({clothes: "shirt", fruit: "apple"}, {clothes: "pants", weather: "hot"});
+
 
