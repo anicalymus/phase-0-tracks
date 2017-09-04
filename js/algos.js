900e417efe1844768a4763cf8132eff59a2ab172
anicalymus <anicalymus@gmail.com>
2017-09-03 22:32:48 -0700

    Add random data test

--- js/algos.js
+++ js/algos.js
@@ -28,4 +28,33 @@ checkKeyValPair({day: "sunny", night: "dark"}, {day: "cloudy", night: "dark"});
 
 checkKeyValPair({clothes: "shirt", fruit: "apple"}, {clothes: "pants", weather: "hot"});
 
+// Release 2: Generate Random Test Data
+var generateRandomTestData = (lengthOftestDataArr) => {
+  var letters = ['a', 'g', 't', 'k', 'p'];
+  var testData = [];
+
+  for (var i = 0; i < lengthOftestDataArr; i++) {
+    var wordLength = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    var newWord = [];
+    for (var k = 0; k <= wordLength; k++) {
+      var letterIndex = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
+      newWord.push(letters[letterIndex]);
+    }
+    testData.push(newWord.join(''));
+  }
+  return testData;
+};
+
+// Driver Code
+
+  generateRandomTestData(4)
+  generateRandomTestData(5)
+  generateRandomTestData(9)
+  generateRandomTestData(3)
+  generateRandomTestData(8)
+  generateRandomTestData(10)
+  generateRandomTestData(1)
+  generateRandomTestData(2)
+  generateRandomTestData(6)
+  generateRandomTestData(7)
 
