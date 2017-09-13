75f8bf2fca020d644282d578d5b2fa506fff371f
anicalymus <anicalymus@gmail.com>
2017-09-13 11:30:40 -0700

    Add routes and parameters

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -1,6 +1,7 @@
 # require gems
 require 'sinatra'
 require 'sqlite3'
+require 'sinatra/reloader'
 
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
@@ -8,9 +9,9 @@ db.results_as_hash = true
 # write a basic GET route
 # add a query parameter
 # GET /
-get '/' do
-  "#{params[:name]} is #{params[:age]} years old."
-end
+# get '/' do
+#   "#{params[:name]} is #{params[:age]} years old."
+# end
 
 # write a GET route with
 # route parameters
@@ -18,6 +19,30 @@ get '/about/:person' do
   person = params[:person]
   "#{person} is a programmer, and #{person} is learning Sinatra."
 end
+
+# write a GET route that displays an address
+get '/contact/:address' do
+  address = params[:address]
+  "Please contact me at #{address}"
+end
+
+# write a GET route that takes a person's name as a query parameter
+get '/' do
+  name = params[:name]
+  if name
+  "Good job, #{name}!"
+  else
+  "Good job!"
+  end
+end
+
+# A route that uses route parameters to add two numbers and respond with the result
+get '/add/:int1/:int2' do
+  sum = params[:int1].to_i + params[:int2].to_i
+  "#{sum}"
+end
+
+
 
 get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
@@ -44,3 +69,18 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# Make a route that allows the user to search the database in some way
+get '/students/:name' do
+  student = db.execute("SELECT * FROM students WHERE name=?", [params[:name]])
+  student.to_s
+  # "Name: #{student['name']}"
+end
+
+# Is Sinatra the only web app library in Ruby? What are some others?
+  # -Other web apps in Ruby include: Padrino, Cramp, Ruby on Rails, Rack, Cuba, Merb etc.
+# Are SQLite and the sqlite3 gem your only options for using a database with Sinatra? What are some others?
+  # -Other gems to use for running a database with Sinatra include: Active Record, CouchDB, Data Mapper, Mongo etc.
+# What is meant by the term web stack?
+  # -Web stack is the collection of software required for Web development. Web stack contains an operating
+  # system, a programming language database software and a Web server.
