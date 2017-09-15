c8461d05df89d975db77b8022725e075913fbe8c
anicalymus <anicalymus@gmail.com>
2017-09-15 15:00:50 -0700

    Add jquery

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,5 +1,13 @@
-document.ready(function(){
-	document.getElementById("disappear").click(function(){
-		(this).hide();
-	});
-});
+function mouseOver() {
+  document.getElementById("text").style.color = "pink";
+}
+
+function mouseOut() {
+	document.getElementById("text").style.color = "blue";
+}
+
+
+var header = document.getElementById("text");
+header.addEventListener("mouseout", mouseOut);
+header.addEventListener("mouseover", mouseOver);
+
